8ad022fa9ed62e05138184591fdb0df003dfab5d
Alex Binguy (M423951) <albinguy@airfrance.fr>
2023-09-11 14:00:15 +0200

    offline mode

--- all-dogs/ViewModel/BreedListViewModel.swift
+++ all-dogs/ViewModel/BreedListViewModel.swift
@@ -6,18 +6,31 @@
 //
 
 import SwiftUI
+import CoreData
 
 class BreedListViewModel: ObservableObject {
     @Published var breeds: [String] = []
     
     func fetchBreeds() {
-        // TODO: si pas de réseau ou erreur : retourner le cache
-        
-        guard let url = URL(string: "https://dog.ceo/api/breeds/list/all") else { return }
         print("fetching all breeds...")
+        
+        fetchBreedsFromUrl() { success in
+            if !success {
+                print("Unable to fetch breed from URL. Let's try in local")
+                self.fetchBreedsFromLocal()
+            }
+        }
+    }
+    
+    func fetchBreedsFromUrl(completion: @escaping (_ success: Bool) -> ()) {
+        guard let url = URL(string: "https://dog.ceo/api/breeds/list/all") else {
+            completion(false)
+            return
+        }
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data, error == nil else {
                 print("Error fetching breeeds: \(error?.localizedDescription ?? "Unknown error")")
+                completion(false)
                 return
             }
             
@@ -28,19 +41,54 @@ class BreedListViewModel: ObservableObject {
                     name
                 } ?? []
                 
-                self.saveBreeds()
+                print("\(breedNames.count) breeds found from URL")
+                self.saveBreeds(breedNames)
                 
                 DispatchQueue.main.async {
                     self.breeds = breedNames
                 }
+                completion(true)
             } else {
+                completion(false)
                 print("No breed found")
             }
         }.resume()
     }
     
-    // TODO: save in database
-    func saveBreeds() {
+    func fetchBreedsFromLocal() {
+        let context = PersistenceController.shared.container.viewContext
+        do {
+            let storedBreeds = try context.fetch(Breed.fetchRequest())
+            print("\(storedBreeds.count) breeds found from local storage")
+            
+            DispatchQueue.main.async {
+                self.breeds = storedBreeds.compactMap{ $0.name }
+            }
+        } catch {
+            print("Error while trying to fetch breed from local storage")
+        }
+    }
+    
+    func saveBreeds(_ breedNames: [String]) {
+        // Delete existing breeds
+        let context = PersistenceController.shared.container.viewContext
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: Breed.self))
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        do {
+            try context.execute(deleteRequest)
+        } catch {
+            print("Error while trying to truncate breed table")
+        }
         
+        for breedName in breedNames {
+            let breedEntity = NSEntityDescription.entity(forEntityName: String(describing: Breed.self), in: context)!
+            let storedBreed = NSManagedObject(entity: breedEntity, insertInto: context) as? Breed
+            storedBreed?.name = breedName
+        }
+        do {
+            try context.save()
+        } catch {
+            print("Error while trying to save the breed")
+        }
     }
 }
--- all-dogs/ViewModel/DogImageListViewModel.swift
+++ all-dogs/ViewModel/DogImageListViewModel.swift
@@ -6,12 +6,12 @@
 //
 
 import Foundation
+import CoreData
 
 struct DogImage: Hashable {
     let imageName: String
     let imageData: Data
 }
-
 
 class DogImageListViewModel: ObservableObject {
     let breed: String
@@ -22,12 +22,26 @@ class DogImageListViewModel: ObservableObject {
     }
     
     func fetchImages() {
-        guard let url = URL(string: "https://dog.ceo/api/breed/\(breed)/images") else { return }
+        print("fetching all dogs for breed \(breed)...")
+        fetchImagesFromUrl() { success in
+            if !success {
+                print("Unable to fetch breed from URL. Let's try in local")
+                self.fetchImagesFromLocal()
+            }
+        }
+    }
+    
+    func fetchImagesFromUrl(completion: @escaping (_ success: Bool) -> ()) {
+        guard let url = URL(string: "https://dog.ceo/api/breed/\(breed)/images") else {
+            completion(false)
+            return
+        }
         
         print("fetching images for \(breed)...")
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data, error == nil else {
                 print("Error fetching images: \(error?.localizedDescription ?? "Unknown error")")
+                completion(false)
                 return
             }
             
@@ -35,27 +49,87 @@ class DogImageListViewModel: ObservableObject {
                 resource.message.forEach { imageUrlString in
                     guard let imageUrl = URL(string: imageUrlString) else {
                         print("Unable to parse image URL: \(imageUrlString)")
+                        completion(false)
                         return
                     }
+                    completion(true)
                     URLSession.shared.dataTask(with: imageUrl) { imageData, _, error in
                         guard let imageData = imageData, error == nil else {
                             print("Error fetching image \(imageUrl) : \(error?.localizedDescription ?? "Unknown error")")
+                            // No completion here since we don't want to go to offline mode only beacause one image fetching has failed
                             return
                         }
                         
+                        let dogImage = DogImage(imageName: imageUrlString, imageData: imageData)
+                        self.save(image: dogImage)
+                        
                         DispatchQueue.main.async {
-                            let dogImage = DogImage(imageName: imageUrlString, imageData: imageData)
                             self.dogImages.append(dogImage)
                         }
                     }.resume()
                 }
             } else {
                 print("No image found")
+                completion(false)
             }
             
         }.resume()
         
     }
     
+    func fetchImagesFromLocal() {
+        let context = PersistenceController.shared.container.viewContext
+        do {
+            let request = NSFetchRequest<NSManagedObject>(entityName: String(describing: Dog.self))
+            request.predicate = NSPredicate(format: "breed = %@", breed)
+            let result = try context.fetch(request)
+            if let storedDogs = result as? [Dog] {
+                print("\(storedDogs.count) dogs found from local storage for breed \(breed)")
+                DispatchQueue.main.async {
+                    self.dogImages = storedDogs.compactMap {
+                        if let imageName = $0.imageName, let imageData = $0.imageData {
+                            return DogImage(imageName: imageName, imageData: imageData)
+                        } else {
+                            print("Incoherent data from local storage. Cannot be displayed correctly")
+                            return nil
+                        }
+                    }
+                }
+            } else {
+                print("No dog found for breed \(breed)")
+            }
+        } catch {
+            print("Error while trying to fetch dogs from local storage for breed \(breed)")
+        }
+    }
     
+    func save(image: DogImage) {
+        // Delete existing image
+        let context = PersistenceController.shared.container.viewContext
+        let request = NSFetchRequest<NSManagedObject>(entityName: String(describing: Dog.self))
+        request.predicate = NSPredicate(format: "imageName = %@", image.imageName)
+        do {
+            let result = try context.fetch(request)
+            if let dogs = result as? [Dog] {
+                for dog in dogs {
+                    context.delete(dog)
+                }
+            }
+        } catch {
+            print("Error while trying to remove dog from local storage")
+        }
+        
+        // Save image
+        let dogEntity = NSEntityDescription.entity(forEntityName: String(describing: Dog.self), in: context)!
+        let storedDog = NSManagedObject(entity: dogEntity, insertInto: context) as? Dog
+        storedDog?.breed = breed
+        storedDog?.imageName = image.imageName
+        storedDog?.imageData = image.imageData
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error while trying to save a dog \(image.imageName)")
+        }
+    }
 }
